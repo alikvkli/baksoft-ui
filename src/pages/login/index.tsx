915e48d107761fee7d937b21adcf988cc301cf7a
ali.kavakli <ali.kavakli4598@gmail.com>
2024-08-01 22:22:40 +0300

    responsive görünüm ayarlamaları yapıldı

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,23 +1,50 @@
+import { useState, useEffect } from "react";
 import Logo from "@/assets/logo.svg";
 import { IoPersonAddOutline } from "react-icons/io5";
 import Login_Image from "@/assets/login-illustrator.svg";
 import Apple from "@/assets/apple.svg";
 import Facebook from "@/assets/Facebook.svg";
 import Google from "@/assets/google.svg";
-import { FaCheck } from "react-icons/fa6";
-import { GiCancel } from "react-icons/gi";
 import { Input } from "@/components/Input";
+import { Link } from "react-router-dom";
+import { HiOutlineMenu } from "react-icons/hi";
+import classNames from "classnames";
+import { IoCloseOutline } from "react-icons/io5";
+
 export default function Login() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  useEffect(() => {
+    if (menuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
+
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  }, [menuOpen]);
+
   return (
     <main>
-      <header className="h-[84px] px-8 py-4 border-b border-[#EAEAEA] shadow-md flex items-center justify-between">
+      <header className="sticky bg-white top-0 z-20 h-[84px] px-8 py-4 border-b border-[#EAEAEA] shadow-md flex items-center justify-between">
         <div className="flex items-center gap-4">
           <img src={Logo} alt="logo" width={25} height={30} />
           <p>
             BAK<span className="font-semibold">soft</span>
           </p>
         </div>
-        <div className="flex items-center gap-4">
+        <div className="hidden max-sm:flex items-center gap-4">
+          <button onClick={toggleMenu} className="text-xl">
+            <HiOutlineMenu />
+          </button>
+        </div>
+        <div className="flex items-center gap-4 max-sm:hidden">
           <button className="w-28 h-[38px] bg-[#F48023] rounded-md text-xs font-bold text-white py-3 px-5 flex items-center justify-center gap-4 hover:bg-[#F48023]/80">
             <IoPersonAddOutline size={13} /> Kayıt Ol
           </button>
@@ -26,17 +53,46 @@ export default function Login() {
           </button>
         </div>
       </header>
+      <div className="fixed inset-0 z-30 transition-all transform">
+        <div className={classNames("absolute inset-0 bg-black transition-opacity duration-300", {
+          "opacity-50": menuOpen,
+          "opacity-0": !menuOpen
+        })} onClick={toggleMenu}></div>
+        <div className={classNames("absolute top-0 right-0 h-full w-3/4 bg-white shadow-md z-40 transition-transform transform", {
+          "translate-x-0": menuOpen,
+          "translate-x-full": !menuOpen
+        })}>
+          <div className="flex flex-col p-4">
+            <button onClick={toggleMenu} className="self-end mb-4">
+              <IoCloseOutline size={24} />
+            </button>
+            <button className="w-full h-[38px] bg-[#F48023] rounded-md text-xs font-bold text-white py-3 px-5 flex items-center justify-center gap-4 hover:bg-[#F48023]/80">
+              <IoPersonAddOutline size={13} /> Kayıt Ol
+            </button>
+            <button className="w-full h-[38px] bg-[#EAEAEA] rounded-md text-xs font-bold text-[#1682FD] py-3 px-5 flex items-center justify-center gap-4 hover:bg-[#EAEAEA]/80 mt-2">
+              Giriş
+            </button>
+          </div>
+        </div>
+      </div>
       <div className="flex h-screen justify-between">
-        <div className="flex flex-col justify-center items-center w-[40%]">
-          <div className="flex flex-col justify-center w-[380px] gap-4">
+        <div className="flex flex-col justify-center shrink-0 items-center w-[40%] max-lg:w-full  max-sm:h-fit max-md:px-4 pt-4">
+          <div className="flex flex-col justify-center w-[380px] max-sm:w-full gap-4">
             <p className="font-bold text-2xl">Giriş Yap</p>
 
-            <Input placeholder="Kullanıcı adı" id="username" type="text" icon={FaCheck} iconColor="#17A100" />
-            <Input placeholder="Şifre" id="password" type="password" icon={GiCancel} iconColor="#FF0000" />
+            <Input
+              placeholder="Kullanıcı adı"
+              id="username"
+              type="text"
+            />
+            <Input
+              placeholder="Şifre"
+              id="password"
+              type="password"
+            />
 
-            <label className="font-normal text-xs text-[#808080] flex justify-end pr-2">
-              Şifremi unuttum
-            </label>
+            <Link className="font-normal text-xs text-[#808080] flex justify-end pr-2" to="/sifremi-unuttum">Şifremi unuttum</Link>
+
             <button className="bg-[#F48023] min-h-10 w-full text-white rounded-md">
               Giriş
             </button>
@@ -48,7 +104,7 @@ export default function Login() {
             </div>
           </div>
         </div>
-        <div className="bg-gradient-to-b from-orange-300 to-orange-500  w-[60%] flex items-center justify-center">
+        <div className="bg-gradient-to-b from-orange-300 to-orange-500  w-[60%] max-lg:hidden  flex items-center justify-center">
           <img src={Login_Image} alt="Login_Image" width={632} height={632} />
         </div>
       </div>
